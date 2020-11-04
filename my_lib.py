c75631fed5224fa4a6c42594b53b3e7c48bbe691
Viciooo <72527323+Viciooo@users.noreply.github.com>
2020-11-05 00:22:45 +0100

    Update my_lib.py

--- my_lib.py
+++ my_lib.py
@@ -1,9 +1,9 @@
 #Moduł przydatnych funkcji
 
-def Split(word): 
+def Split(word): #dzieli string na znaki 
     return [char for char in word]
 
-def StringCompare(string1,string2):
+def StringCompare(string1,string2): #porównywanie stringów
     
     cnt = 0
     l1 = split(string1)
@@ -24,7 +24,7 @@ def StringCompare(string1,string2):
 
     print("Są podobne do :", cnt, "miejsca")
 
-def InsertionSort(arr): 
+def InsertionSort(arr): #sortowanie
     for i in range(1, len(arr)):  
         key = arr[i] 
         j = i-1
@@ -33,14 +33,14 @@ def InsertionSort(arr):
             j -= 1
         arr[j+1] = key 
 
-def GenRndTab(length,start,end):
+def GenRndTab(length,start,end): #generuje tablicę jednowymiarową
     from random import randint
     tab = []
     for i in range(length):
         tab.append(randint(start,end))
     return tab
 
-def IfPrime(n):
+def IfPrime(n): #sprawdza czy pierwsza
     k = 2
 
     if(n==1):
@@ -58,7 +58,7 @@ def IfPrime(n):
 
     return True
 
-def ToBinary(n,length):
+def ToBinary(n,length):#zamienia liczbę na binarną 
     l =[]
     while n // 2 != 0:
         l.append(n%2)
@@ -68,7 +68,7 @@ def ToBinary(n,length):
         l.append(0)
     return l
 
-def DivList(n):
+def DivList(n): #zwraca listę podzielników w postaci tablicy
     l = []
     for i in range(2,n+1):
         if n % i == 0:
@@ -76,3 +76,63 @@ def DivList(n):
         while n % i == 0:
             n //= i
     return l
+
+def GenRnd2DmArr(N,start,end): #generuje dwuwymiarowe tablice
+    from random import randint
+
+    t = N*[0]
+    c = 0
+    r = 0
+
+    for i in range(N):
+        t[i] = N*[0]
+        
+    while t[N-1][N-1] == 0:
+        if c==N:
+            r += 1
+            c = 0
+        t[r][c] = randint(start,end)
+        c += 1
+    
+    return t
+
+def RowSum(tab): #sumuje wiersze
+    N = len(tab)
+    suma = 0
+    for i in range(N):
+        suma += tab[i]
+    return suma
+
+def FlipTheTab(tab): #obraca tablicę kolumny to teraz wiersze itd
+    r = 0 
+    c = 0
+    y = 0
+    suma = 0
+    N = len(tab)
+    new_tab = [0]*N
+
+    for i in range(N):
+        new_tab[i] = N*[0]
+    for c in range(N):
+        x = 0
+        for r in range(N):
+            new_tab[y][x] = tab[r][c]
+            x += 1
+        y += 1
+    return new_tab
+
+def SearchForBorS(tab,bORs): # biggest or smallest - szuka największego lub najmniejszego el tablicy
+    N = len(tab)
+    i_saver = 0
+    val_saver = tab[0]
+    if bORs == "b":
+        for i in range(1,N):
+            if tab[i] > val_saver:
+                 i_saver = i
+                 val_saver = tab[i]
+    if bORs == "s":
+        for i in range(1,N):
+            if tab[i] < val_saver:
+                 i_saver = i
+                 val_saver = tab[i]
+    return i_saver
