8200cfd3ee82732ca43598322982d3bbd63a7400
Viciooo <72527323+Viciooo@users.noreply.github.com>
2020-12-29 11:27:50 +0100

    q

--- zestaw_7/ex22.py
+++ zestaw_7/ex22.py
@@ -1,30 +1,31 @@
 # Zadanie 22. Dana jest lista, który być może zakończona jest cyklem. Napisać funkcję, która sprawdza ten fakt.
 
 class Node:
-    def __init__(self,val=None,next=None):
+    def __init__(self,val=None,idx=0,next=None):
         self.next = next
         self.val = val
+        self.idx = idx
 
 def is_cycle(first):
-    p = first
+    p, prev = first.next, first
     while p != None:
-        if p.next == first:
+        if p.idx < prev.idx:
             return True
-        p = p.next
+        p,prev = p.next,p
     return False
 
 # def pushBack(first,n):
 #     p, previous = first, None
 #     while p != None:
 #         p, previous = p.next, p
-#     previous.next = Node(n)
+#     previous.next = Node(n,previous.idx+1)
 #     return first
 
 # def pushBackAddCycle(first,n):
 #     p, previous = first, None
 #     while p != None:
 #         p, previous = p.next, p
-#     q = Node(n)
+#     q = Node(n,previous.idx+1)
 #     previous.next = q
 #     q.next = first
 #     return first
@@ -39,6 +40,6 @@ def is_cycle(first):
 # z = pushBack(z,3)
 # z = pushBack(z,5)
 # z = pushBack(z,7)
-# #z = pushBackAddCycle(z,2)
+# z = pushBackAddCycle(z,2)
 
 # print(is_cycle(z))
---
+++ zestaw_7/ex32.py
@@ -0,0 +1,4 @@
+# Zadanie 32. Lista reprezentuje wielomian o współczynnikach całkowitych. Elementy w liście ułożone są według rosnących potęg.
+# Proszę napisać funkcję obliczającą różnicę dwóch dowolnych wielomianów. Wielomiany reprezentowane są przez wyżej opisane listy.
+# Procedura powinna zwracać wskaźnik do nowo utworzonej listy reprezentującej wielomian wynikowy.
+# Listy wejściowe powinny pozostać niezmienione.
