80da88680dfb37899ceccc526395d025a2eed70e
Viciooo <72527323+Viciooo@users.noreply.github.com>
2020-12-29 10:57:09 +0100

    hello there

--- zestaw_7/ex23.py
+++ zestaw_7/ex23.py
@@ -1,15 +1,43 @@
 # Zadanie 23. Dana jest lista, który zakończona jest cyklem. Napisać funkcję, która zwraca liczbę elementów w cyklu.
 
 class Node:
-    def __init__(self,val=None,next=None):
+    def __init__(self,val=None,idx=0,next=None):
         self.next = next
         self.val = val
+        self.idx = idx
 
-def countCycleLen(first):
-    p, cnt = first, 0
-    while p != None:
-        cnt += 1
-        if p.next == first:
-            return cnt
-        p = p.next
-    return "to nie jest cykl :("
+def getCycleLen(first): 
+#jeśli zakładamy, że może to nie być cykl to dodajemy p != none
+    p, prev = first.next, first
+    while p.idx > prev.idx:
+        p, prev = p.next, p
+    return prev.idx - p.idx + 1
+
+# def pushBack(first,n):
+#     p, prev = first, None
+#     while p != None:
+#         p, prev = p.next, p
+#     prev.next = Node(n,prev.idx+1)
+#     return first
+
+# def pushBackAddCycle(first,n):
+#     p, previous = first, None
+#     while p != None:
+#         p, previous = p.next, p
+#     q = Node(n,previous.idx+1)
+#     previous.next = q
+#     q.next = first
+#     return first
+
+# z = Node(1)
+# #z = pushBack(z,3)
+# #z = pushBack(z,3)
+# z = pushBackAddCycle(z,5)
+# print(getCycleLen(z))
+
+# def write(first):
+#     while first != None:
+#         print("[",first.val,first.idx,"] -----> ",end='',sep='')
+#         first = first.next
+#     print(None)
+
--- zestaw_7/ex24.py
+++ zestaw_7/ex24.py
@@ -1,2 +1,56 @@
 # Zadanie 24. Dana jest lista, który zakończona jest cyklem. Napisać funkcję, która zwraca liczbę elementów przed cyklem.
-#? nie wiem czym się różni od 23
+
+class Node:
+    def __init__(self,val=None,idx=0,next=None):
+        self.next = next
+        self.val = val
+        self.idx = idx
+
+def getLenNoCycle(first): #długość przed cyklem
+#jeśli zakładamy, że może to nie być cykl to dodajemy p != none
+    p, prev = first.next, first
+    while p.idx > prev.idx:
+        p, prev = p.next, p
+    return p.idx 
+
+def pushBack(first,n):
+    p, prev = first, None
+    while p != None:
+        p, prev = p.next, p
+    prev.next = Node(n,prev.idx+1)
+    return first
+
+def pushBackAddCycle(first,n):
+    p, previous = first, None
+    while p != None:
+        if p.idx == 2:
+            m = p
+        p, previous = p.next, p
+    q = Node(n,previous.idx + 1)
+    previous.next = q
+    q.next = m
+    return first
+
+def getCycleLen(first): 
+#jeśli zakładamy, że może to nie być cykl to dodajemy p != none
+    p, prev = first.next, first
+    while p.idx > prev.idx:
+        p, prev = p.next, p
+    print(p.idx, prev.idx)
+    return prev.idx - p.idx + 1
+
+z = Node(1)
+z = pushBack(z,3)
+z = pushBack(z,3)
+z = pushBack(z,3)
+z = pushBackAddCycle(z,5)
+print(getCycleLen(z))
+print(getLenNoCycle(z))
+
+def write(first):
+    while first != None:
+        print("[",first.val,first.idx,"] -----> ",end='',sep='')
+        first = first.next
+    print(None)
+
+# write(z)
