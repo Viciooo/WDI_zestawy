8736ad11a96f61e303c1b24a72fa1cbf5b7ec086
Viciooo <72527323+Viciooo@users.noreply.github.com>
2020-12-29 11:19:40 +0100

    Update test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-def strCompare(sInput,sOfList):
-    for i in range(len(min(sInput,sOfList))):
-        if ord(sInput[i:i+1]) < ord(sOfList[i:i+1]):
-            return True
-        if ord(sInput[i:i+1]) > ord(sOfList[i:i+1]):
-            return False
-
-print(strCompare("zbc","cd"))
